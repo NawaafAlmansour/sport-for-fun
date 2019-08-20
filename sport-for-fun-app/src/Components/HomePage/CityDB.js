2dfc5cd6b33f17e7480e6efa867f9973bd3a453a
aisha <aishaalshuraim@gmail.com>
2019-08-20 14:17:14 +0300

    Add city data

---
+++ sport-for-fun-app/src/Components/HomePage/CityDB.js
@@ -0,0 +1,57 @@
+const CityDB = {
+
+  cities: [
+    {
+      "Name": "Riyadh",
+      "lan": 24.631034,
+      "lat":46.716762,
+      "centers": [
+        {
+        "lan":24.664371,
+        "lat":46.754235,
+        "BannerImg":11,
+        "sportKind":"Horse riding school",
+        "centerName":"Al-Aghar Equestrian Club",
+        "description":"Amongst the well-established riding schools in Saudi Arabia, Riyadh is characterised by an ideal combination of well-trained horses / ponies, skilled instructors, and picturesque open fields. Known to provide friendly horse riding lessons to schools and families. We also organise pony trekking trips and arrange children’s’ pony parties.",
+        "images":["https://lh5.googleusercontent.com/p/AF1QipNVmAOG74KJFETB0ORV13km8QtlqJV5EDnjcV2W=s901-k-no","https://lh5.googleusercontent.com/p/AF1QipP3dAcFhDw50A0faXkDkcWQ7Cz8qyWpgkJcvy77=w203-h270-k-no","https://lh5.googleusercontent.com/p/AF1QipNkhdvJKif7uDlO1yoYHFW_TWAmaIQqby7y3YoB=w203-h115-k-no"],
+        "contact": "0114812200",
+        "comments":"Enjoyable horse riding with kids",
+        "visitors":["Kids","Adults"],
+        "address":["P.O. Box 84329","Riyadh 11671","Kingdom of Saudi Arabia"],        "addressLink":"https://goo.gl/maps/APxqhg6MxWFqhTLq9",
+        "openingHours":"8AM,8PM",
+        "activities":"Horse riding"
+        },
+          {
+        "lan":24.684212,
+        "lat":46.724742,
+        "BannerImg":11,
+        "sportKind":"Tennis club",
+        "centerName":"Saudi Table Tennis Federation",
+        "description":"Table tennis, also known as ping-pong, is a sport in which two or four players hit a lightweight ball back and forth across a table using small rackets. The game takes place on a hard table divided by a net. Except for the initial serve, the rules are generally as follows: players must allow a ball played toward them to bounce one time on their side of the table, and must return it so that it bounces on the opposite side at least once. A point is scored when a player fails to return the ball within the rules. Play is fast and demands quick reactions. Spinning the ball alters its trajectory and limits an opponent's options, giving the hitter a great advantage.",
+        "images":["https://fastly.4sqi.net/img/general/200x200/15400278_cQiac3vzckkyx6xbog6l6ZYA8_xIGRoUvzM0UNGR2j0.jpg","http://international.alhussan.edu.sa/userfiles/2018/2/posts/Albums/postsAlbum_5536_postsID_2659_513818863.jpg","https://www.google.com/imgres?imgurl=https%3A%2F%2Ffastly.4sqi.net%2Fimg%2Fgeneral%2F200x200%2F18227603_c4-cFzebKFTdKuwtXfD8DCUwuMetCSjoWvMTjPSCRXc.jpg&imgrefurl=https%3A%2F%2Ffoursquare.com%2Fv%2Fsaudi-table-tennis-federation%2F52b42428498ea869641d5917&docid=DZcMqsC3EQTRjM&tbnid=Nptur9FWJOOsgM%3A&vet=10ahUKEwjT8q3KnpHkAhUPyxoKHVIDBcwQMwhIKBEwEQ..i&w=200&h=200&safe=strict&bih=719&biw=1325&q=saudi%20table%20tennis%20federation&ved=0ahUKEwjT8q3KnpHkAhUPyxoKHVIDBcwQMwhIKBEwEQ&iact=mrc&uact=8"],
+        "contact": "0114812240",
+        "comments":"Good place for table tennis and other indoor stuff.",
+        "visitors":["Kids","Adults"],
+        "address":["P.O. Box 84329","Riyadh 11671","Kingdom of Saudi Arabia"],        "addressLink":"https://goo.gl/maps/APxqhg6MxWFqhTLq9",
+        "openingHours":"8AM,8PM",
+        "activities":"Table Tennis"
+        },
+        ]
+      
+    },
+    {
+        "Name": "Jeddah",
+        "lan": 24.631034,
+        "lat":46.716762,   
+    },
+    {
+        "Name": "Abha",
+        "lan": 24.631034,
+        "lat":46.716762,
+    }
+ 
+  
+  ]
+}
+
+export default CityDB;
